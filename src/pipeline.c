43f75baf0bc3a99f8e1b25e3a8b618be5c15f42a
Harshad <hbhandwaldar@gmail.com>
2022-03-07 22:18:07 -0500

    v1

--- src/pipeline.c
+++ src/pipeline.c
@@ -3,13 +3,14 @@
 #include<stdbool.h>
 #include "pipelineHeader.h"
 
-
+// Initialize pipe line to blank state
 void initializePipe()
 {
     for (int i = 0; i < SIZE; i++)
         pipeStage[i] = 99;
 }
 
+// Function to check if the pipline is empty or there any jobs ongoing, If yes, move thoes jobs.
 void LineStatus ()
 {
     printf("\n Inside LineStatus");
@@ -33,7 +34,6 @@ void LineStatus ()
             clkCycle();
         else 
         {
-            printPipe();
             while (status == 0)
             {
             pipeStage[0] = nextOrder(order);
@@ -42,7 +42,7 @@ void LineStatus ()
         }
 }
 
-
+// Fetch next order from trace and check the item. Pass the item to pipeline
 int nextOrder(int order)
 {
     fscanf(fetchRequest,"%s",buffer);
@@ -75,13 +75,18 @@ int nextOrder(int order)
         }
 }
 
+// At each cycle check the pipeline stages and shift to next stage
 void clkCycle()
 {
     int IP, temp = pipeStage[SIZE-1];
     for (IP = SIZE-1; IP > 0; IP--)
     {
-        if(IP == 0 && baking_count == 1000)
-            bakery_time = bakery_time+10;
+        if(IP == 0)
+            for(int k = 0; k<SIZE && baking_count == 1000; k++) 
+            {
+                pipeStage[k] = pipeStage[k-1];
+                bakery_time++;
+            }
         if(IP == 9 && baking_count == 10)
             bakery_time = bakery_time + 1;
         pipeStage[IP] = pipeStage[IP-1];
@@ -109,7 +114,7 @@ int main(int argc, char *argv[])
     printf("No request: %d\n", no_request);
 
     printf("\nHow many minutes to bake: %d\n", bakery_time);
-    //performance = baking_count/bakery_time;
+    performance = (float)baking_count/bakery_time;
     printf("\nPerformance (bakes/minutes): %.2f\n", performance);
 
     return 0;
